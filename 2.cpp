245193b3f195b666587b16509a0972c1d244e8c2
anyapetrenko <91524835+anyapetrenko@users.noreply.github.com>
2021-12-07 11:32:21 +0200

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,408 @@
-system("cls");
+#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <io.h>
+#include <fcntl.h>
+
+using namespace std;
+
+struct Date
+{
+  int day,
+    month,
+    year;
+};
+
+struct DataOfUser {
+    char name[256], surname[256],
+     address[256],
+     phoneNumber[256];
+    Date dateOfBirth;
+    int idNetwork;
+    int postalCode;
+    int id;
+};
+
+class UserDirectory {
+protected:
+    DataOfUser data;
+
+public:
+    UserDirectory() {}
+    ~UserDirectory() {}
+
+    void SetData(DataOfUser newData) {
+        data = newData;
+    }
+
+    DataOfUser GetData() {
+        return data;
+    }
+
+    void Print() {
+        cout << "  ID: " << data.id << endl;
+        cout << "  Имя: " << data.name << endl;
+        cout << "  Фамилия: " << data.surname << endl;
+        cout << "  Адрес: " << data.address << endl;
+        cout << "  Номер телефона (мобильный): " << data.phoneNumber << endl;
+        cout << "  Дата рождения ~ДД.ММ.ГГГГ~: " << data.dateOfBirth.day << ' ' << data.dateOfBirth.month << ' ' << data.dateOfBirth.year << endl;
+        cout << "  Почтовый индекс: " << data.postalCode << endl;
+        cout << "  ID соц сети: " << data.idNetwork << endl;
     }
 };
 
-class DataBase {
+struct DataOfNetwork {
+    char name[256], version[256];
+    char country[256];
+    char owner[256];
+    Date dateOfRelease;
+    int yearOfLastUpdate;
+    int id;
+};
+
+class NetworkDirectory {
+protected:
+    DataOfNetwork data;
+
+public:
+    NetworkDirectory(){}
+    ~NetworkDirectory() {}
+
+    void SetData(DataOfNetwork newData) {
+        data = newData;
+    }
+
+    DataOfNetwork GetData() {
+        return data;
+    }
+
+    void Print() {
+        cout << "  ID: " << data.id << endl;
+        cout << "  Название соцсети: " << data.name << endl;
+        cout << "  Версия:: " << data.version << endl;
+        cout << "  Регион: " << data.country << endl;
+        cout << "  Владелец: " << data.owner << endl;
+        cout << "  День выпуска ~ДД.ММ.ГГГГ~: " << data.dateOfRelease.day << ' ' << data.dateOfRelease.month << ' ' << data.dateOfRelease.year << endl;
+        cout << "  Последнее обновление (год): " << data.yearOfLastUpdate << endl;
+    }
+};
+
+// Ввод данных
+        cout << "  Название соцсети: ";
+        std::cin.ignore();
+        cin >> buf.name;
+        cout << "  Версия: ";
+        cin >> buf.version;
+        cout << "  Регион: ";
+        cin >> buf.country;
+        cout << "  Владелец: ";
+        cin >> buf.owner;
+        cout << "  День выпуска ~ДД.ММ.ГГГГ~: ";
+        cin >> buf.dateOfRelease.day >> buf.dateOfRelease.month >> buf.dateOfRelease.year;
+        cout << "  Последнее обновление (год): ";
+        cin >> buf.yearOfLastUpdate;
+
+        id = LastID();
+        buf.id = ++id;
+
+        system("cls");
+
+        // Запись в конец файла
+        ofstream save(fname, ios::binary | ios::app);
+        save.write((char*)&buf, sizeof(buf));
+        save.close();
+
+        cout << "Данные добавлены!" << endl;
+    }
+
+    int LastID() {
+        DataOfNetwork buf;
+        ifstream file(fname, ios::binary | ios::in);
+
+        // Если файл не открылся
+        if (!file) {
+            cout << "Ошибка открытия файла класса NetworkBase!" << endl;
+            return -1;
+        }
+
+        // Выбор последнего ID
+        while (!file.eof()) {
+            file.read((char*)&buf, sizeof(DataOfNetwork));
+        }
+        file.close();
+
+        return buf.id;
+    }
+
+    int Count() {
+        DataOfNetwork buf;
+        ifstream file(fname, ios::binary | ios::in);
+
+        // Если файл не открылся
+        if (!file) {
+            cout << "Ошибка открытия файла класса NetworkBase!" << endl;
+            return -1;
+        }
+
+        // Подсчет количества данных в файле
+        count = 0;
+        while (!file.eof()) {
+            file.read((char*)&buf, sizeof(DataOfNetwork));
+            count++;
+        }
+        file.close();
+
+        count--; // так как в файле 1 пустая строка в конце, подсчет данных укажет на 1 больше, чем в действительности
+        if (count < 0)
+            count = 0;
+
+        return count;
+    }
+
+    void Print() {
+        // Проверка наличия данных
+        if (IsEmpty()) {
+            cout << "Данные пусты!" << endl;
+            return;
+        }
+
+        DataOfNetwork buf;
+        ifstream load(fname, ios::binary | ios::in);
+
+        // Если файл не открылся
+        if (!load) {
+            cout << "Ошибка открытия файла класса NetworkBase!" << endl;
+            return;
+        }
+
+        count = Count();
+
+        // Поэлементное чтение и вывод
+        for (int i = 0; i < count; i++)
+        {
+            load.read((char*)&buf, sizeof(DataOfNetwork));
+            data.SetData(buf);
+            data.Print();
+            cout << "-------------------------" << endl;
+        }
+        load.close();
+    }
+
+    void Clear() {
+        ifstream file(fname, ios::binary | ios::out);
+        file.clear();
+        file.close();
+    }
+
+    bool IsEmpty() {
+        count = Count();
+        return count == 0;
+    }
+
+    void Edit() {
+        // Проверка наличия данных
+        if (IsEmpty()) {
+            cout << "Данные пусты!" << endl;
+            return;
+        }
+
+        // Выбор позиции
+        int pos;
+        int id;
+        cout << "Укажите ID: " << endl;
+        cin >> id;
+
+        pos = PosByID(id);
+        system("cls");
+
+        // Проверка выбранной позиции
+        if (pos < 0 || pos > count - 1) {
+            cout << "Позиция выбрана неверно!" << endl;
+            return;
+        }
+
+        DataOfNetwork buf;
+
+        // Ввод данных
+        cout << "  Название соцсети: ";
+        std::cin.ignore();
+        cin >> buf.name;
+        cout << "  Версия: ";
+        cin >> buf.version;
+        cout << "  Регион: ";
+        cin >> buf.country;
+        cout << "  Владелец: ";
+        cin >> buf.owner;
+        cout << "  День выпуска ~ДД.ММ.ГГГГ~: ";
+        cin >> buf.dateOfRelease.day >> buf.dateOfRelease.month >> buf.dateOfRelease.year;
+        cout << "  Последнее обновление (год): ";
+        cin >> buf.yearOfLastUpdate;
+        system("cls");
+
+        buf.id = id;
+
+// Запись в указанное место в файле
+        ofstream save(fname, ios::binary | ios::in);
+        save.seekp(sizeof(buf) * pos, ios::beg);
+        save.write((char*)&buf, sizeof(buf));
+        save.close();
+
+        cout << "Данные изменены!" << endl;
+    }
+
+    void Del() {
+        // Проверка наличия данных
+        if (IsEmpty()) {
+            cout << "Данные пусты!" << endl;
+            return;
+        }
+
+        // Выбор позиции
+        int pos;
+        int id;
+        cout << "Укажите ID: " << endl;
+        cin >> id;
+
+        system("cls");
+
+        pos = PosByID(id);
+        pos++;
+
+        // Проверка выбранной позиции
+        if (pos < 0 || pos > count - 1) {
+            cout << "Позиция выбрана неверно!" << endl;
+            return;
+        }
+
+        // Новый вариант удаления
+
+        // Читаем файл с места следующего после удаления
+        DataOfNetwork buf;
+        ifstream load(fname, ios::binary | ios::in);
+        load.seekg(sizeof(buf) * pos, ios::beg);
+
+        // Записываем на место удаляемого
+        ofstream save(fname, ios::binary | ios::in);
+        save.seekp(sizeof(buf) * (pos - 1), ios::beg);
+
+        int biteSize = 0;
+        for (int i = pos; i < count; i++) {
+            load.read((char*)&buf, sizeof(DataOfNetwork));
+            save.write((char*)&buf, sizeof(buf));
+        }
+
+        load.close();
+        save.close();
+
+        // Открываем файл для обрезания не нужной части
+        int fh, result;
+        if (_sopen_s(&fh, fname.c_str(), _O_RDWR | _O_CREAT, _SH_DENYNO,
+            _S_IREAD | _S_IWRITE) == 0)
+        {
+            cout << "Размер файла до удалени: " << _filelength(fh) << endl;
+
+            if ((result = _chsize(fh, sizeof(DataOfNetwork) * (count - 1))) == 0)
+                cout << "Размер файла изменен..." << endl;
+            else
+                cout << "Ошибка изменения размера файла!" << endl;
+
+            cout << "Текущий размер файла: " << _filelength(fh) << endl;
+            _close(fh);
+        }
+
+        cout << "Данные удалены!" << endl;
+    }
+
+    void Search() {
+        // Проверка наличия данных
+        if (IsEmpty()) {
+            cout << "Данные пусты!" << endl;
+            return;
+        }
+
+        int id;
+        cout << "Укажите ID для поиска: ";
+        cin >> id;
+
+        system("cls");
+
+        DataOfNetwork buf;
+        ifstream load(fname, ios::binary | ios::in);
+        bool found = false;
+
+        // Если файл не открылся
+        if (!load) {
+            cout << "Ошибка открытия файла класса NetworkBase!" << endl;
+            return;
+        }
+
+        count = Count();
+
+        // Поэлементное чтение и вывод
+        for (int i = 0; i < count; i++)
+        {
+            load.read((char*)&buf, sizeof(DataOfNetwork));
+
+            if (id == buf.id) {
+                data.SetData(buf);
+                data.Print();
+                found = true;
+                break;
+            }
+        }
+        load.close();
+
+        if (!found)
+            cout << "Совпадений не найдено!" << endl;
+    }
+
+    int PosByID(int id) {
+        DataOfNetwork buf;
+        ifstream load(fname, ios::binary | ios::in);
+
+        // Если файл не открылся
+        if (!load) {
+            cout << "Ошибка открытия файла класса NetworkBase!" << endl;
+            return -1;
+        }
+
+        int count = Count();
+        int pos = -1;
+
+        // Поэлементное чтение и вывод
+        for (int i = 0; i < count; i++)
+        {
+            load.read((char*)&buf, sizeof(DataOfNetwork));
+
+            if (id == buf.id) {
+                pos = i;
+                break;
+            }
+        }
+        load.close();
+
+        return pos;
+    }
+
+    void Menu(int& act) {
+        cout << "Выберите действие: " << endl
+            << "(0) Назад" << endl
+            << "(1) Вывод" << endl
+            << "(2) Добавить" << endl
+            << "(3) Изменить" << endl
+            << "(4) Удалить" << endl
+            << "(5) Поиск" << endl
+            << "(6) Очистить" << endl
+            << "(7) Изменить файл" << endl
+            << "Ваш выбор: ";
+        cin >> act;
+
+system("cls");
+    }
+};
+...class DataBase {
 public:
     UserBase users;
     NetworkBase networks;
